e5d369fbde97fc1276480667b744c1005f0856fa
Steve Krenzel <sgk284@gmail.com>
2012-08-28 20:55:17 -0700

    21 & 22 - Be sure to :set nowrap for 22

---
+++ 21/21.hs
@@ -0,0 +1,9 @@
+d n = sum $ filter (/= n) $ concat factors'
+  where factors' = [[x, n `div` x] | x <- [1..(iSqrt n)], (n `mod` x == 0)]
+        iSqrt = floor . sqrt . fromIntegral
+
+isAmicable n = (n /= a) && (n == b)
+  where a = d n
+        b = d a
+
+main = print $ sum $ filter isAmicable [0..9999]
